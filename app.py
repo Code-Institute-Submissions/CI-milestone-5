e0d28d68781a2c49ed24e157d1d34fb52558f9d0
Fred Gryson <fgryson@gmail.com>
2022-01-18 15:44:57 +0000

    Added: Database access and test template

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
+from asyncio import tasks
 import imp
 import os
-from flask import Flask
+from flask import (Flask, flash, render_template,
+                   redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/get_brews")
+def get_brews():
+    brews = mongo.db.Brews.find()
+    return render_template("brews.html", brews=brews)
 
 
 if __name__ == "__main__":
